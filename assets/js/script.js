93f408cd3c2a33098531fb8c85ca9d4c19f6efdd
sahmali <shahmalimh@code.edu.az>
2019-07-23 15:34:39 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//NOTIFICATIONS OWL START
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    mouseDrag:true,
+    dots:false,
+    items:1,
+    singleItem:true,
+    margin:50,
+})
+//NOTIFICATIONS OWL END
