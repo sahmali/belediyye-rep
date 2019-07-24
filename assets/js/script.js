5ef6001fb9a2e100640a855cfcee79444caaae12
sahmali <shahmalimh@code.edu.az>
2019-07-24 13:31:58 +0300

    main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //NOTIFICATIONS OWL START
-$('.owl-carousel').owlCarousel({
+$('.not-carousel .owl-carousel').owlCarousel({
     loop:true,
     margin:0,
     nav:true,
@@ -9,4 +9,19 @@ $('.owl-carousel').owlCarousel({
     singleItem:true,
     margin:50,
 })
+
+
 //NOTIFICATIONS OWL END
+
+//MAIN SLIDER OWL START
+$('.main-carousel .owl-carousel').owlCarousel({
+    stagePadding: 0,
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+    singleItem:true
+})
+
+
+/*CALENDAR*/
